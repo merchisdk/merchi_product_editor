f62e1a13ac8b2691fcb035121df973a8037226a6
freyaLi321 <freyali0321@gmail.com>
2025-04-09 14:33:50 +1000

    add imageGallery, adjust styles

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as ProductEditor } from './components/ProductEditor';
-export { default as ImageGallery } from './components/ImageGallery';
-export * from './utils/grid';
-export * from './types';
-import './styles/index.css';
